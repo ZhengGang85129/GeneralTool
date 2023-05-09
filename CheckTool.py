c0947913a406d6ab4f2fe43edcff96c2885d8f64
zhenggan <zheng-gang.chen@cern.ch>
2023-05-09 11:11:43 +0200

    [Add function evaluation]

--- CheckTool.py
+++ CheckTool.py
@@ -1,7 +1,53 @@
 import os,sys 
+import math
+import time
+import tracemalloc
+import os ,sys
 CURRENT_WORKDIR = os.getcwd()
 sys.path.append(CURRENT_WORKDIR)
 from GeneralTool.Colors import Colors
+
+
+def convert_size(size_bytes) -> str:
+    if size_bytes == 0:
+        return "0B"
+    size_name = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "{} {}".format(s, size_name[i])
+
+
+
+def MemoryMeasure(Function_Name="", verbose = False)->None:
+    def Inner(func):
+        def MemoryMeasure_Wrapper(*args, **kwargs):
+            tracemalloc.start()
+            func(*args, **kwargs)
+            current, peak = tracemalloc.get_traced_memory()
+            if verbose:
+                print(ShellStyles.HighlightStyle() + '---------------------------------------------------------------' + ShellStyles.NormalStyle())
+                print('current memory size:' + ShellStyles.ResultStyle() + f' {convert_size(current)}' +  ShellStyles.NormalStyle() + ', peak memory size:'+ ShellStyles.ResultStyle()  +f' {convert_size(peak)} '+ ShellStyles.NormalStyle()  +'after Function '+ ShellStyles.CyanStyle() +f'`{Function_Name}`'+ ShellStyles.NormalStyle())
+                if Function_Name == "":
+                    print(ShellStyles.WarningLabel() + 'Function name is not specified in MemoryMeasure')
+        return MemoryMeasure_Wrapper
+    
+    return Inner
+
+
+def TimeMeasure(Function_Name="", verbose = False)->None:
+    def Inner(func):
+        def TimeMeasure_Wrapper(*args, **kwargs):
+            time0 = time.time()
+            func(*args, **kwargs)
+            if verbose:
+                print(f'Run Time for Function '+ ShellStyles.CyanStyle()  +f'`{Function_Name}`'+ ShellStyles.NormalStyle() + ' is :'+ ShellStyles.SuccessStyle() +f'{time.time()-time0:.2f} sec' + ShellStyles.NormalStyle())
+                if Function_Name == "":
+                    print(ShellStyles.WarningLabel() + 'Function name is not specified in TimeMeasure')
+        return TimeMeasure_Wrapper
+    return Inner
+
+
 def CheckDir(Dir_to_check="",MakeDir=True,quiet=False):
     '''
         Just check whether a folder is existed or not. If it is not, then create a new one.
